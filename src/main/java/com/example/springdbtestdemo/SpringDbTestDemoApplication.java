c6f9aced22c2553bedfa1f4cc8bef3d4a3cbe4eb
Kleber Vales <klebervales.dev@gmail.com>
2025-08-13 16:12:38 -0300

    implement spring boot with junit 5 and Kotlin DSL

---
+++ src/main/java/com/example/springdbtestdemo/SpringDbTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdbtestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbTestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDbTestDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springdbtestdemo/SpringDbTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdbtestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDbTestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
