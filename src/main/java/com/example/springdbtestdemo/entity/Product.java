1081b36751d7b659f404c5c639d8ca434f17844e
Kleber Vales <klebervales.dev@gmail.com>
2025-08-13 16:48:31 -0300

    This causes Hibernate to create the tables before data.sql runs.

--- src/main/java/com/example/springdbtestdemo/entity/Product.java
+++ src/main/java/com/example/springdbtestdemo/entity/Product.java
@@ -1,21 +1,21 @@
 package com.example.springdbtestdemo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(name = "product") // força nome exato
 @Getter
 @Setter
 public class Product {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "price")
     private double price;
 }
